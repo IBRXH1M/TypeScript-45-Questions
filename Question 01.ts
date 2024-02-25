73767ec37452f1ed49c6894e53889d9b41a90ebe
Muhammad Ibrahim Khan <149921040+IBRXH1M@users.noreply.github.com>
2024-02-25 22:57:07 +0500

    Update Question 01.ts

--- Question 01.ts
+++ Question 01.ts
@@ -1,3 +1,3 @@
 // Install Node.js, TypeScript and VS Code on your computer.
-
-console.log("I Have Downloaded Node.js , TypeScript and Vs Code On My Computer.")
+//Answer 01
+console.log("I Have Downloaded Node.js , TypeScript and Vs Code On My Computer.");
