b50f6ea8f61de4d894596c7bec1343eff483dab6
Muhammad Ibrahim Khan <149921040+IBRXH1M@users.noreply.github.com>
2024-02-25 23:00:59 +0500

    Update Question 09.js

--- Question 09.js
+++ Question 09.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 function q9() {
-    let favoriteNumber = 7;
+    let favoriteNumber = 9;
     let message = "My favorite number is " + favoriteNumber + ".";
     console.log(message);
 }
