deabbb60a3d378feaf6bbab275ca8dd75a989157
Muhammad Ibrahim Khan <149921040+IBRXH1M@users.noreply.github.com>
2024-02-25 23:01:11 +0500

    Update Question 09.ts

--- Question 09.ts
+++ Question 09.ts
@@ -1,6 +1,6 @@
 // Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 export default function q9() {
-    let favoriteNumber = 7;
+    let favoriteNumber = 9;
     let message = "My favorite number is " + favoriteNumber + ".";
     console.log(message);
   }
