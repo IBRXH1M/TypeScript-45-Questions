9edba6188f64bb8ee59562818eb98ff67e29bbd9
Muhammad Ibrahim Khan <149921040+IBRXH1M@users.noreply.github.com>
2024-02-25 22:59:14 +0500

    Update Question 02.ts

--- Question 02.ts
+++ Question 02.ts
@@ -1,7 +1,7 @@
 // Q2 : Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 //Answer 02
 
-let name1: string = "Abdul Hannan";
+let name1: string = "Muhammad Ibrahim Khan";
 console.log('Hello ' + name1 +' , would you like to learn some Python today?');
 
 export{}
