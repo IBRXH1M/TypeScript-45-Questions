48b6f56f530343521eed63a7f1464201cbe08e78
Muhammad Ibrahim Khan <149921040+IBRXH1M@users.noreply.github.com>
2024-02-25 23:01:50 +0500

    Update Question 10.js

--- Question 10.js
+++ Question 10.js
@@ -2,10 +2,10 @@
 // Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
 Object.defineProperty(exports, "__esModule", { value: true });
 // Favorite Number Program
-// Author: Abdul Hannan
+// Author: Muhammad Ibrahim Khan
 // Date: 2023-02-09
 function q10() {
-    let favoriteNumber = 7;
+    let favoriteNumber = 9;
     let message = `My favorite number is ${favoriteNumber}.`;
     console.log(message);
 }
